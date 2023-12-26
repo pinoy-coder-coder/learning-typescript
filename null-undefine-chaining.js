03bb16def95cf96132de0f1ded90a20311e29d42
wayne <obesowayne@gmail.com>
2023-12-26 12:04:15 +0800

    update for null coalescing

---
+++ null-undefine-chaining.js
@@ -0,0 +1,42 @@
+var _a, _b, _c;
+//type null and undefine
+var title = null;
+// console.log(title);
+if (!title) {
+    console.log("this is not null statement");
+}
+else {
+    console.log("this a null statement");
+}
+//type chaining with the nested 
+var course = {
+    addtxt: {
+        title: "learning typescript journey",
+    }
+};
+// console.log(`the course: ${course.title}`);
+if ((_a = course === null || course === void 0 ? void 0 : course.addtxt) === null || _a === void 0 ? void 0 : _a.title) {
+    console.log("the course: ".concat(course.addtxt.title));
+}
+else {
+    console.log("the course is hard");
+}
+//type chaining with null coalescing
+var korso = {
+    addtxt: {
+        titolo: "learning typescript journey"
+    }
+};
+var balita = (_c = (_b = korso === null || korso === void 0 ? void 0 : korso.addtxt) === null || _b === void 0 ? void 0 : _b.titolo) !== null && _c !== void 0 ? _c : "no title found";
+logCourseTitle(korso);
+function logCourseTitle(korso) {
+    var _a;
+    //validation for error null coalecing
+    if (!(korso === null || korso === void 0 ? void 0 : korso.addtxt)) {
+        console.log("text field error!");
+    }
+    if ((_a = korso === null || korso === void 0 ? void 0 : korso.addtxt) === null || _a === void 0 ? void 0 : _a.titolo) {
+        console.log("the title is: " + balita);
+    }
+}
+;
